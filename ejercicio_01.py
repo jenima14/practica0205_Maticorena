52dd0be05af18fa54c4315b6e9a0319055e27494
jenima14 <jmarticarr2@educacion.navarra.es>
2023-11-17 16:44:07 +0100

    ejercicio_01

---
+++ ejercicio_01.py
@@ -0,0 +1,14 @@
+def funcion_saludo(nombre):
+    """Muestra un saludo con el nombre proporcionado.
+
+    Parametros:
+    nombre (str): Nombre de la persona a saludar.
+
+    Salida:
+    Returns
+    None - ausencia de valor."""
+    print("¡Hola " + nombre + "!")
+
+# solución de la función
+cadena_nombre = "Jenifer"
+funcion_saludo(cadena_nombre)
